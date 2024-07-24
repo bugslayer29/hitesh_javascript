22ad6f9809c94d49c848c76adc9e2161e5652a4c
bugslayer29 <rohitprem2003kr@gmail.com>
2024-07-24 14:58:39 +0530

    clear

--- test.js
+++ test.js
@@ -1,3 +1,19 @@
 console.log(
  "javascript"
 )
+
+const accountId = 144553
+let accountEmail = "aryan29@gmail.com"
+var accountPassword = "12345"
+accountCity =  "patna"
+ let accountState
+
+// accountId = 2 
+
+console.log(accountId);
+console.log(accountCity);
+
+console.table ( [accountEmail, accountId, accountPassword, accountCity , accountState])
+
+// prefer not to use "var" because of the issue of block scope anf fumctiional scopr.
+
