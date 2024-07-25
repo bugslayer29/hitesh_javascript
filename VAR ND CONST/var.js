85a35c13a9323b57d91fa04d43720c1ae2f1b05a
bugslayer29 <rohitprem2003kr@gmail.com>
2024-07-25 14:31:03 +0530

    variable and constant files added to HITESH-JAVASCRIPT

---
+++ VAR ND CONST/var.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+// cont is a keyword which means if you assign  a value to const keyword you can't change it. Basically, it is constant(which can't be changed)so, you can't write another id like accountId = 2(it will give you error)
+let accountEmail = "rohit29@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+
+accountEmail = "aryan@gamil.com"
+accountPassword = "4567555"
+accountCity = "patna"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
