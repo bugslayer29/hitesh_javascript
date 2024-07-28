f76d6c57c40d0424e22518517b8bf952c06f0d7b
bugslayer29 <rohitprem2003kr@gmail.com>
2024-07-28 17:44:13 +0530

    file add

---
+++ Conversion-operation/operation.js
@@ -0,0 +1,16 @@
+/* 
+    console.log(2+2) output = 4;
+      console.log(2-2) output = 0;
+      console.log(2*2) output = 4;
+        console.log(2/2) output = 1;
+      console.log(2**3), it means 2 raise to the power 3. output = 8;
+        console.log(2%2) it is modulus operator,it always gives you remainder which is  = ;0.
+       
+        */
+// we can add one string to another string (only add not subtract) like :
+let str1 = "rohit"
+let str2 = "kumar"
+ 
+let str3 = str1 + str2
+
+console.log(str1 + " " + str2)
